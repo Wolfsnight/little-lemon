394bbe55c3bae49fe39f8613cc6943c031e375a8
Markus Spiegler <Spiegler@hotmail.de>
2025-04-29 09:44:54 +0200

    2025-04-29 - UPDATE - Login

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,6 +5,7 @@ from .serializers import MenuSerializer
 from rest_framework import viewsets
 from .models import Booking
 from .serializers import BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -23,3 +24,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
