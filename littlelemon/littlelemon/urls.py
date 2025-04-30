5933375ec8d9bb741f40e8dd7b8f982cef912f1e
Markus Spiegler <Spiegler@hotmail.de>
2025-04-30 08:25:42 +0200

    2025-04-30 - UPDATE - Final

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -9,8 +9,8 @@ router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/booking/', include(router.urls)),
-    path('restaurant/', include('restaurant.urls')),
+    path('booking/', include(router.urls)),
+    path('', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('api/menu-items/', views.MenuItemsView.as_view(), name='menu_items'),
     path('api/menu-items/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu_item_detail'),
 ]
